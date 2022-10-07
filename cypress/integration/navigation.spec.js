b16b9cae31b02de680db52ca2abd3ac1da65ab65
NadiaPia <206i417@gmail.com>
2022-10-07 00:37:51 -0700

    Testing with the Cypress framework added

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,20 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("li[data-testid=day]", 'Tuesday') //we are identifying that we want the <li> element that contains "Tuesday"
+      .click()    
+      .should("have.class", "day-list__item--selected");
+
+      /*cy.visit("/");      
+      cy.contains("[data-testid=day]", "Tuesday")       
+      .click()       
+      .should("have.class", "day-list__item--selected");*/
+  }) 
+
+
+  
+});
